1cf4d99ac61815ea7f05f56216ddccf94cc1f8b0
lichun <lichun@dianhun.cn>
2017-07-20 17:30:04 +0800

    Add graph visualizer

---
+++ Assets/Cinemachine/Timeline/CameraSwitcher.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CameraSwitchPlayable : PlayableBehaviour
+{
+    public Camera camera;
+    private bool m_WasEnabled;
+
+    public override void OnGraphStart(Playable playable)
+    {
+        base.OnGraphStart(playable);
+        if (camera != null)
+        {
+            m_WasEnabled = camera.enabled;
+        }
+    }
+
+    public override void OnGraphStop(Playable playable)
+    {
+        base.OnGraphStop(playable);
+        if (camera != null)
+        {
+            camera.enabled = m_WasEnabled;
+        }
+    }
+
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (camera != null)
+        {
+            camera.enabled = false;
+        }
+    }
+
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if (camera != null)
+        {
+            camera.enabled = true;
+        }
+    }
+
+}
+
+[System.Serializable]
+public class CameraSwitcher : PlayableAsset
+{
+    public ExposedReference<Camera> camera;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<CameraSwitchPlayable>.Create(graph);
+        playable.GetBehaviour().camera = camera.Resolve(graph.GetResolver());
+        return playable;
+    }
+}
+
---
+++ Assets/Cinemachine/Timeline/CameraSwitcherTrack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+
+[Serializable]
+[TrackClipType(typeof(CameraSwitcher))]
+[TrackMediaType(TimelineAsset.MediaType.Script)]
+[TrackColor(0.0f, 1.0f, 0.0f)]
+public class CameraSwitcherTrack : TrackAsset
+{
+    
+}
